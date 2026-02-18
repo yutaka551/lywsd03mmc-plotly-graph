efbf8d42e9fe3d4cb9a490f2514d7a33b0b2079c
yutaka551 <yutaka551@users.noreply.github.com>
2026-02-19 00:07:58 +0900

    Update plugin version and requirements to 0.1.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ plugin_package = "octoprint_plotly_graph_lywsd03mmc"
 plugin_name = "OctoPrint-PlotlyGraph-LYWSD03MMC"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "1.0.0"
+plugin_version = "0.1.0"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
@@ -32,7 +32,7 @@ plugin_url = "https://github.com/yutaka551/lywsd03mmc-plotly-graph"
 plugin_license = "AGPLv3"
 
 # Any additional requirements besides OctoPrint should be listed here
-plugin_requires = ["lywsd03mmc>=1.1.1"]
+plugin_requires = ["lywsd03mmc>=0.1.0"]
 
 # --------------------------------------------------------------------------------------------------------------------
 # More advanced options that you usually shouldn't have to touch follow after this point
